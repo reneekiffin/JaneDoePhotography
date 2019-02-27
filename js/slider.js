1cd7c32d4988d2ad7607689f48410145663e4f50
Renee <reneekiffin@outlook.com>
2019-02-26 23:33:46 -0500

    Adjustments for operation
    modifications to exclude php files and functons

---
+++ js/slider.js
@@ -0,0 +1,13 @@
+
+  var instance = M.Carousel.init({
+    fullWidth: true,
+    indicators: true
+  });
+
+  // Or with jQuery
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+      
